86de6920430c89b69ee2fae2e447b4312b1a9bba
dawnzs01 <18013865675@163.com>
2023-10-20 18:47:40 +0800

    jdk8u-fix

--- data-prepper-plugins/opensearch/src/main/java/org/opensearch/dataprepper/plugins/sink/opensearch/index/IndexManager.java
+++
@@ -1,8 +0,0 @@
-package org.opensearch.dataprepper.plugins.sink.opensearch.index;
-
-import java.io.IOException;
-
-public interface IndexManager{
-	void setupIndex() throws IOException;
-	String getIndexName(final String indexAlias) throws IOException;
-}
